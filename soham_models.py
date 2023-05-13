fd6c2c666131e83d000ba87c757f68bf71eac5d9
sk0107 <sohamkonar28@gmail.com>
2023-05-12 17:50:11 -0700

    made soham files based on charlie files

---
+++ soham_models.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+import soham_utils as utils
+import clean_data as clean
+
+# To run a model on a specified auction type, pick from the following as the parameter for df:
+# 'df_all'
+# 'four_week'
+# 'eight_week'
+# 'thirteen_week'
+# 'seventeen_week'
+# 'twenty_six_week'
+# 'fifty_two_week'
+# 'cmb'
+
+def get_linear_regression_model(df):
+    x_train, y_train, x_test, y_test = utils.split_train_test(df, 0.7)
+    x_train = np.delete(x_train, 0, axis=1) # delete the time data
+    x_test = np.delete(x_test, 0, axis=1)
+
+    # Fit the regression
+    model = LinearRegression()
+    model.fit(x_train, y_train)
+    model.predict(x_test)
+    r_sq = model.score(x_test, y_test)
+    print(f"coefficient of determination: {r_sq}")
+    print(f"intercept: {model.intercept_}")
+    print(f"slope: {model.coef_}")
+
+def get_arima_model(df):
+    train, test = utils.split_train_test(df, 0.7, split_xy=False)
+
+    train['date'] = pd.to_datetime(train['date'])
+    train = train.set_index('date')
+
+    print(train)
+
+    model = sm.tsa.ARIMA(train, order=(1, 1, 1)).fit()
+
+    model.predict(test)
+
+    print(model(summary))
+
+get_arima_model('four_week')
---
+++ soham_utils.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import charlie_clean_data as clean_data
+import math
+
+def get_auction_type_df(df):
+    df_all = clean_data.clean_main_data()
+    bond_types = ['Security term_4 WK', 'Security term_8 WK', 'Security term_13 WK', 'Security term_17 WK', 'Security term_26 WK', 'Security term_52 WK', 'Security term_CMB']
+    if (df == 'df_all'):
+        return df_all
+    elif (df == 'four_week'):
+        df_all = df_all[df_all['Security term_4 WK'] == 1]
+    elif (df == 'eight_week'):
+        df_all = df_all[df_all['Security term_8 WK'] == 1]
+    elif (df == 'thirteen_week'):
+        df_all = df_all[df_all['Security term_13 WK'] == 1]
+    elif (df == 'seventeen_week'):
+        df_all = df_all[df_all['Security term_17 WK'] == 1]
+    elif (df == 'twenty_six_week'):
+        df_all = df_all[df_all['Security term_26 WK'] == 1]
+    elif (df == 'fifty_two_week'):
+        df_all = df_all[df_all['Security term_52 WK'] == 1]
+    elif (df == 'cmb'):
+        df_all = df_all[df_all['Security term_CMB'] == 1]
+    df_all = df_all.drop(bond_types, axis=1)
+    return df_all
+
+def create_arrays(df):
+    # Create variables to predict based on
+    selected_columns = ['date', 'Total issue', '(SOMA) Federal Reserve banks', 'Depository institutions', 'Individuals', 'Dealers and brokers',
+                        'Pension and Retirement funds and Ins. Co.', 'Investment funds', 'Foreign and international', 'Other and Noncomps', 'News Sentiment']
+    X_array = np.array(df[selected_columns].values.tolist())
+    Y_array = np.array(df['Auction high rate %'].values.tolist()).T
+    return X_array, Y_array
+
+def split_train_test(df, split_size, split_xy=True):
+    ts = get_auction_type_df(df)
+
+    # Split into a training set and a testing set
+    train_size = int(len(ts) * split_size)
+    train_ts, test_ts = ts[:train_size], ts[train_size:]
+
+    # Splits training and testing sets into x and y
+    x_train, y_train = create_arrays(train_ts)
+    x_test, y_test = create_arrays(test_ts)
+
+    if (split_xy):
+        return x_train, y_train, x_test, y_test
+    return train_ts, test_ts
+
+def create_data(df):
+    X_array, Y_array = clean_data.create_arrays(df)
+    final_array = np.column_stack((X_array, Y_array))
+
+    dates = final_array[:, 0]
+    difference = pd.Series(dates)-dates[0]
+
+    df_diff = pd.DataFrame(difference) #find diff since original date
+    df_diff['weeks'] = df_diff / pd.Timedelta(weeks=1) # convert to weeks
+    weeks_array = pd.DataFrame(df_diff['weeks'])
+    weeks_array = np.array(weeks_array.values.tolist()) # convert back to numpy
+
+    final_array = np.delete(final_array, 0, axis=1) # remove the original datetime column
+
+    # Concatenate along the second axis (i.e. columns)
+    final_array = np.concatenate((weeks_array, final_array), axis=1)
+    final_array = final_array[final_array[:, 0].argsort()]
+    
+    print(final_array)
+    return final_array
+
+split_train_test('four_week', 0.7, split_xy=False)
