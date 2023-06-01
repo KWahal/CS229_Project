9a50fc1ca50e78c82d6918c2fbe3311287ced64c
sk0107 <sohamkonar28@gmail.com>
2023-06-01 15:48:08 -0700

    made changes to lstm

--- lstm.py
+++ lstm.py
@@ -13,41 +13,51 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scalecast.Forecaster import Forecaster
 
+
+TEST_LENGTH = 120 # original: 120
+FUTURE_DATES = 120 # original: 120
+EPOCHS = 15 # original: 15
+LAYERS = 6 # original: 3
+
 def get_LSTM_model(df):
-    train, test = utils.split_train_test(df, 0.8, split_xy=False)
+    train, test = utils.split_train_test(df, 1, split_xy=False)
     f = Forecaster(
     y=train['Auction high rate %'],
     current_dates=train['date'],
-    test_length = 12,
-    future_dates = 12,
+    test_length = TEST_LENGTH,
+    future_dates = FUTURE_DATES,
     cis = False,
     )
     # f.plot()
-    plt.title('Orig Series',size=16)
-   # plt.show()
+    # plt.title('Orig Series',size=16)
+    # plt.show()
 
-    figs, axs = plt.subplots(2, 1,figsize=(12,6))
-    f.plot_acf(ax=axs[0],lags=36)
-    f.plot_pacf(ax=axs[1],lags=36)
-   # plt.show()
+    # figs, axs = plt.subplots(2, 1,figsize=(12,6))
+    # f.plot_acf(ax=axs[0],lags=36)
+    # f.plot_pacf(ax=axs[1],lags=36)
+    # plt.show()
     stat, pval, _, _, _, _ = f.adf_test(full_res=True)
     print("stat is " + str(stat))
     print("pval is " + str(pval))
 
-    f.set_test_length(12)       # 1. 12 observations to test the results
-    f.generate_future_dates(12) # 2. 12 future points to forecast
+    f.set_test_length(TEST_LENGTH)       # 1. 12 observations to test the results
+    f.generate_future_dates(FUTURE_DATES) # 2. 12 future points to forecast
     f.set_estimator('lstm')
     f.manual_forecast(
     lags=36,
     batch_size=32,
-    epochs=15,
+    epochs=EPOCHS,
     validation_split=.2,
     activation='tanh',
     optimizer='Adam',
     learning_rate=0.001,
-    lstm_layer_sizes=(100,)*3,
-    dropout=(0,)*3,
+    lstm_layer_sizes=(100,)*LAYERS,
+    dropout=(0,)*LAYERS,
     )
-    #f.plot_test_set(ci=True)
+    f.plot_test_set()
+    plt.savefig('images/lstm.png')
+    f.plot_test_set()
+    plt.show()
+    
 
 get_LSTM_model('df_all')
