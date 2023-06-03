1f91e655b9ce5411250300116b86fdc1df2c344e
KWahal <karsen.wahal@gmail>
2023-06-02 17:32:42 -0700

    proph

--- prophet.py
+++ prophet.py
@@ -13,6 +13,7 @@ def prophet(df):
     data = np.column_stack((X_train, y_train))
     data = pd.DataFrame(data)
     data = data.rename(columns={0: "ds", 1: "y"})
+    print(data)
 
     m = NeuralProphet(n_forecasts = 60, n_lags=60, n_changepoints=50, yearly_seasonality=False, weekly_seasonality=False, 
                       daily_seasonality=False, batch_size=64, epochs=50, learning_rate=1.0)
